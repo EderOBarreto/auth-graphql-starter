e28c637ed26b715adbac2ef04dd9eacd0b9d7c73
eder <ederoliveirabarreto@gmail.com>
2021-11-17 09:40:42 -0300

    Update libs and create usertype

---
+++ server/schema/mutations.js
@@ -0,0 +1,24 @@
+const { GraphQLObjectType, GraphQLString } = require("graphql");
+const UserType = require("./types/user_type");
+const { signup, login } = require("../services/auth");
+const {
+  moduleIds,
+} = require("babel-core/lib/transformation/file/options/config");
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parentValue, { email, password }, req) {
+        return signup({ email, password, req });
+      },
+    },
+  },
+});
+
+module.exports = mutation;
