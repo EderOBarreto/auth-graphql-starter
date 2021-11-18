4fc0f5d9b2e0dd7f755c237510403e9bb7c193f2
eder <ederoliveirabarreto@gmail.com>
2021-11-18 09:01:19 -0300

    Add Login Form and mutation

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { graphql } from "react-apollo";
+import { Link } from "react-router";
+import query from "../queries/CurrentUser";
+import mutation from "../mutations/Logout";
 
-const Header = () => {
-  return <div>HEADER</div>;
+const Header = ({ data, mutate }) => {
+  const onLogoutClick = () => {
+    mutate({ refetchQueries: [{ query }] });
+  };
+
+  const renderButtons = () => {
+    const { loading, user } = data;
+    if (loading) return <div />;
+    if (user) {
+      return (
+        <li>
+          <a onClick={onLogoutClick}>Logout</a>
+        </li>
+      );
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <nav className="">
+      <div className="nav-wrapper">
+        <Link className="brand-logo left" to="/">
+          Home
+        </Link>
+        <ul className="right">{renderButtons()}</ul>
+      </div>
+    </nav>
+  );
 };
 
-export default Header;
+export default graphql(mutation)(graphql(query)(Header));
